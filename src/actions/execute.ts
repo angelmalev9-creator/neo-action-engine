cd59749d73cbeac6c4a1f956c09259755f4eb845
angelmalev9-creator <angelmalev9@gmail.com>
2026-01-24 22:28:58 +0200

    Update execute.ts

--- src/actions/execute.ts
+++ src/actions/execute.ts
@@ -1,14 +1,15 @@
 import { bookAction } from "./book.js";
+import { checkAvailability } from "./availability.js";
 
 export async function executeAction(action: any) {
   switch (action.type) {
+    case "availability":
+      return checkAvailability(action.payload);
+
     case "booking":
       return bookAction(action.payload);
 
-    // case "add_to_cart":
-    // case "availability":
-
     default:
-      throw new Error("Unsupported action");
+      throw new Error("Unsupported action type");
   }
 }
