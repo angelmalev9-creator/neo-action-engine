3e90e39d776abb844f356637427a63f125020b37
angelmalev9-creator <angelmalev9@gmail.com>
2026-01-24 21:45:35 +0200

    Update getBrowser.ts

--- src/browser/getBrowser.ts
+++ src/browser/getBrowser.ts
@@ -1,1 +1,13 @@
+import { chromium, Browser } from "playwright";
 
+let browser: Browser | null = null;
+
+export async function getBrowser() {
+  if (!browser) {
+    browser = await chromium.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+  }
+  return browser;
+}
