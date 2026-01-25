17be12ef133d0a293b65a2f36780411504674f2a
angelmalev9-creator <angelmalev9@gmail.com>
2026-01-26 01:45:40 +0200

    Update execute.ts

--- src/actions/execute.ts
+++ src/actions/execute.ts
@@ -4,6 +4,7 @@ import { withBrowser } from "../browser/session.js";
 import { checkAvailability } from "./availability.js";
 import { autoAvailability } from "./autoAvailability.js";
 import { exploreBooking } from "./exploreBooking.js";
+import { exploreBookingSystematic } from "./exploreBookingSystematic.js";
 import { book } from "./book.js";
 
 export async function executeAction(
@@ -27,6 +28,9 @@ export async function executeAction(
 
       case "explore_booking":
         return exploreBooking(page, params);
+
+      case "explore_booking_systematic":
+        return exploreBookingSystematic(page, params);
 
       case "book":
         return book(page, params);
